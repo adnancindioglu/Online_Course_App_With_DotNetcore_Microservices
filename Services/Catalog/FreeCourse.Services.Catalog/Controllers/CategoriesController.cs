2786d0f4c8c1ca8e22bba5aaaaea9ca00d7dbcdd
adnan cindioglu <adnancindioglu@hotmail.com>
2022-02-27 15:46:33 +0300

    include project controller

---
+++ Services/Catalog/FreeCourse.Services.Catalog/Controllers/CategoriesController.cs
@@ -0,0 +1,48 @@
+﻿using FreeCourse.Services.Catalog.Dtos.CreateDto;
+using FreeCourse.Services.Catalog.Services;
+using FreeCourse.Shared.ControllerBases;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    internal class CategoriesController : CustomBaseController
+    {
+        private ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var response = await _categoryService.GetAllAsync();
+
+            return CreateActionResultInstance(response);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(string id)
+        {
+            var response = await _categoryService.GetById(id);
+
+            return CreateActionResultInstance(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CategoryCreateDto categoryCreateDto)
+        {
+            var response = await _categoryService.CreateAsync(categoryCreateDto);
+
+            return CreateActionResultInstance(response);
+        }
+    }
+}
--- Services/Catalog/FreeCourse.Services.Catalog/Services/ICategoryService.cs
+++ Services/Catalog/FreeCourse.Services.Catalog/Services/ICategoryService.cs
@@ -1,4 +1,5 @@
 ﻿using FreeCourse.Services.Catalog.Dtos;
+using FreeCourse.Services.Catalog.Dtos.CreateDto;
 using FreeCourse.Services.Catalog.Models;
 using FreeCourse.Shared.Dtos;
 using System;
@@ -11,7 +12,7 @@ namespace FreeCourse.Services.Catalog.Services
     interface ICategoryService
     {
         Task<Response<List<CategoryDto>>> GetAllAsync();
-        Task<Response<CategoryDto>> CreateAsync(Category category);
+        Task<Response<CategoryDto>> CreateAsync(CategoryCreateDto categoryCreateDto);
         Task<Response<CategoryDto>> GetById(string id)
     }
 }
